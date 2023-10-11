5e7b3f16b9fedc7bffaae6c349220a4f42992625
Rahim <103603909+RahimMammad44@users.noreply.github.com>
2023-10-12 00:49:49 +0400

    js-homework

---
+++ main.js
@@ -0,0 +1,74 @@
+////////////////////////Task 1///////////////////////////
+
+// let sum = 0
+
+//     for (let i = 1; i < 100; i++) {     
+//     if(i % 3 === 0 || i % 5 === 0) {
+//         sum += i
+//     }
+// }
+// console.log(sum)
+
+////////////////////////Task 2///////////////////////////
+
+// const number = parseFloat(prompt("Enter a number:"));
+
+//   if (number < 0) {
+//     alert("Menfi reqemler olmaz!");
+//   } else if (number === 0) {
+//     alert("0 quvvet deyil");
+//   } else {
+//     let square = false;
+//     for (let i = 1; i * i <= number; i++) {
+//       if (i * i === number) {
+//         square = true;
+//         break;
+//       }
+//     }
+    
+//     if (square) {
+//       alert(`${number} is a perfect square.`);
+//     } else {
+//       alert(`${number} is not a perfect square.`);
+//     }
+//   }
+
+
+
+////////////////////////Task 3///////////////////////////
+
+// let number = parseInt(prompt("Enter a number: ")) 
+// let sadeNumber = true;
+
+// for(let i = 2; i < number; i++) {
+//     if(number % i === 0) {
+//         sadeNumber = false;
+//     }
+// }
+
+// if(sadeNumber === true) {
+//     alert("Sade")
+// } else {
+//     alert("Murekkeb")
+// }
+
+////////////////////////Task 4  ///////////////////////////
+
+// let month = window.prompt("Enter a month:");
+// let result;
+
+// switch (month) {
+//   case "December", "January", "February":
+//     result = "Winter";
+//     break;
+//   case "March", "April", "May":
+//     result = "Spring";
+//     break;
+//   case "June", "July", "August":
+//     result = "Summer";
+//     break;
+//   case "September", "October", "November":
+//     result = "Autumn";
+//     break;
+// }
+// console.log(result);
